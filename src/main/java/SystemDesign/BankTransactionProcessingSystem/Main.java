4130213f9c32abc4e4000dcef32b8a5a6b8deacd
knileshh <neutron_stays@protonmail.com>
2025-08-15 11:26:32 +0530

    BankTransactionProcessingSystem INIT

---
+++ src/main/java/SystemDesign/BankTransactionProcessingSystem/Main.java
@@ -0,0 +1,7 @@
+package SystemDesign.BankTransactionProcessingSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bank Transaction Processing System online!");
+    }
+}
